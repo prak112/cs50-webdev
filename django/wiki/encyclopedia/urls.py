09cda96965e1203488b208e64751550e4d95366e
PrakGov <55034062+prak112@users.noreply.github.com>
2023-10-09 19:31:48 +0300

    task attempt-search entries

--- django/wiki/encyclopedia/urls.py
+++ django/wiki/encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.renderhtml, name="renderhtml")    # renders all listed titles and notfound pages
+    path("<str:title>", views.renderhtml, name="renderhtml"),   # renders all listed titles and notfound pages
+    path("<str:q>", views.search, name="search"),
 ]
--- django/wiki/encyclopedia/views.py
+++ django/wiki/encyclopedia/views.py
@@ -1,6 +1,11 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
 
 from . import util
+
+class SearchForm(forms.Form):
+    search = forms.CharField(label="Search here")
 
 
 def index(request):
@@ -28,3 +33,27 @@ def renderhtml(request, title):
                   {"title": util.get_entry(title)})
 
 
+def search(request):
+    """
+    Search for a given query and render the results.
+    """
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+
+        if form.is_Valid():
+            query = form.cleaned_data["search"]
+
+            entries = util.list_entries()
+            results = []
+            for entry in entries:
+                if query.lower() == entry.lower():
+                    return render(request, f"encyclopedia/{entry}.html", context={
+                        "title":util.get_entry(entry)
+                    })
+                elif entry.__contains__(query):
+                    results.append(entry)
+                else:
+                    return render(request, 'encyclopedia/notfound.html', {"title": query.upper()})
+            return render(request, 'encyclopedia/results.html', {
+                "entries": results
+                })

b680249a2de3f287d605f12bd046c7a9b7db8126
PrakGov <55034062+prak112@users.noreply.github.com>
2023-10-09 21:40:32 +0300

    debug new page task

--- django/wiki/encyclopedia/urls.py
+++ django/wiki/encyclopedia/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.renderhtml, name="renderhtml"),   # renders all listed titles and notfound pages
-    path("<str:q>", views.search, name="search"),
+    path("renderhtml/<str:title>", views.renderhtml, name="renderhtml"),   # renders all listed titles and notfound pages
+    # path("<str:q>", views.search, name="search"),
+    path("new_entry", views.new_entry, name="new_entry"),
 ]

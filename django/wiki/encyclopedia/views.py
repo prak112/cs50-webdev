e39b81d8a6dfe51915c0b75605365dd8b8d69864
PrakGov <55034062+prak112@users.noreply.github.com>
2023-10-09 21:40:32 +0300

    new page-debug view

--- django/wiki/encyclopedia/views.py
+++ django/wiki/encyclopedia/views.py
@@ -4,8 +4,10 @@ from django import forms
 
 from . import util
 
-class SearchForm(forms.Form):
-    search = forms.CharField(label="Search here")
+class NewForm(forms.Form):
+    # search = forms.CharField(label="Search")
+    title = forms.CharField(label="Title")
+    content = forms.CharField(widget=forms.Textarea(attrs={"rows":"25", "cols":"50"}))
 
 
 def index(request):
@@ -28,32 +30,47 @@ def renderhtml(request, title):
         None.
     """
     if not util.get_entry(title):
-        return render(request, 'encyclopedia/notfound.html', {"title": title.upper()})
+        return render(request, 'encyclopedia/not_found.html', {"title": title.upper()})
     return render(request, f"encyclopedia/{title}.html",
                   {"title": util.get_entry(title)})
 
 
-def search(request):
-    """
-    Search for a given query and render the results.
-    """
-    if request.method == 'POST':
-        form = SearchForm(request.POST)
+# def search(request):
+#     """
+#     Search for a given query and render the results.
+#     """
+#     if request.method == 'POST':
+#         form = SearchForm(request.POST)
 
-        if form.is_Valid():
-            query = form.cleaned_data["search"]
+#         if form.is_valid():
+#             query = form.cleaned_data["search"]
 
-            entries = util.list_entries()
-            results = []
-            for entry in entries:
-                if query.lower() == entry.lower():
-                    return render(request, f"encyclopedia/{entry}.html", context={
-                        "title":util.get_entry(entry)
-                    })
-                elif entry.__contains__(query):
-                    results.append(entry)
-                else:
-                    return render(request, 'encyclopedia/notfound.html', {"title": query.upper()})
-            return render(request, 'encyclopedia/results.html', {
-                "entries": results
-                })
+#             entries = util.list_entries()
+#             results = []
+#             for entry in entries:
+#                 if query.lower() == entry.lower():
+#                     return render(request, f"encyclopedia/{entry}.html", context={
+#                         "title":util.get_entry(entry)
+#                     })
+#                 elif entry.__contains__(query):
+#                     results.append(entry)
+#                 else:
+#                     return render(request, 'encyclopedia/notfound.html', {"title": query.upper()})
+#             return render(request, 'encyclopedia/results.html', {
+#                 "entries": results
+#                 })
+    
+
+def new_entry(request):
+
+    # if request.method == 'POST':
+    #     form = NewForm(request.POST)
+    #     if form.is_valid():
+    #         title = form.cleaned_data["title"]
+    #         content = form.cleaned_data["content"]
+            
+
+        return render(request, 'encyclopedia/new_entry.html', context={
+            # "form" : form,
+    })
+    

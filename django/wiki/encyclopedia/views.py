c5f835ffb5491f09fe59281afe09d81485d24d30
PrakGov <55034062+prak112@users.noreply.github.com>
2023-10-09 17:19:54 +0300

    solved entry, index, md-html tasks

--- django/wiki/encyclopedia/views.py
+++ django/wiki/encyclopedia/views.py
@@ -8,3 +8,23 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def renderhtml(request, title):
+    """
+    Render HTML page for a given request and title.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+        title (str): The title of the HTML page to render.
+
+    Returns:
+        HttpResponse: The rendered HTML page.
+
+    Raises:
+        None.
+    """
+    if not util.get_entry(title):
+        return render(request, 'encyclopedia/notfound.html', {"title": title.upper()})
+    return render(request, f"encyclopedia/{title}.html",
+                  {"title": util.get_entry(title)})
+
+

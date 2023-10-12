7ad22ec9577f7f37d96a96ef8067ec00041d4e5b
PrakGov <55034062+prak112@users.noreply.github.com>
2023-10-12 19:05:46 +0300

    edit entry-debug double form submit error

--- django/wiki/encyclopedia/views.py
+++ django/wiki/encyclopedia/views.py
@@ -1,3 +1,4 @@
+from django.core import exceptions
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
@@ -5,9 +6,12 @@ from django import forms
 
 from . import util
 
+from django.conf import settings    # for Base Directory setting
+import os           # to join directory path
 import subprocess   # to run CLI command for .md to .html conversion
-import random
-import re   # to extract topic_name for renderhtml view
+import markdown2    # to convert Markdown to HTML
+import random       # for random_page view
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -28,10 +32,20 @@ def renderhtml(request, title):
     Raises:
         None.
     """
-    if not util.get_entry(title):
-        return render(request, 'encyclopedia/not_found.html', {"title": title.upper()})
-    else:
+    try:
         return render(request, f"encyclopedia/{title}.html", {"title": util.get_entry(title), "topic": title.upper()})
+    
+    except exceptions.ObjectDoesNotExist:
+        # convert Markdown to HTML
+        markdown_file = f"entries/{title}.md"   
+        html_file = f"encyclopedia/templates/encyclopedia/{title}.html"
+        cli_command = f"python -m markdown2 {markdown_file} > {html_file}"
+        subprocess.run(cli_command, shell=True, check=True)
+        return render(request, f"encyclopedia/{title}.html", {"title": util.get_entry(title), "topic": title.upper()})
+
+
+    # if util.get_entry(title) is None:
+    #         else:
 
 
 # class definition for creating Form objects for new_entry & edit_entry
@@ -51,14 +65,8 @@ def new_entry(request):
             content = form.cleaned_data["content"]
 
             if util.save_entry(title, content):               
-                # convert Markdown to HTML
-                markdown_file = f"entries/{title}.md"   
-                html_file = f"encyclopedia/templates/encyclopedia/{title}.html"
-                cli_command = f"python -m markdown2 {markdown_file} > {html_file}"
-                subprocess.run(cli_command, shell=True, check=True)
-
                 # redirect to new entry
-                return HttpResponseRedirect(reverse('renderhtml', args=[title]))
+                return renderhtml(request, title)
             
             # return already existing entry
             else:       
@@ -85,23 +93,50 @@ def edit_entry(request, title):
             'content': util.get_entry(title)
                 }
     form = NewForm(initial=intial_data)  
+    # MAJOR DEBUGGING ---
+    # form.is_valid() NOT NECESSARY, form.has_changed IS NECESSARY
+    # Request Type changes from POST to GET -- reasons --
+        # Double-Form Resubmission -> after an error when refreshing the page, the form is submitted again
+                                #  -> the browser interprets this as a GET request and hence changes the Request Type
     
-    if((request.method == "POST") and (form.is_valid())):       # unable to identify why request.method != "POST"
-            title = form.cleaned_data["title"]
-            content = form.cleaned_data["content"]
+
+    # pseudo-code 
+    """
+    1. fill form with initial data
+    2. check if form data has changed
+    3. if form data has changed, check if "POST" request
+    4. if "POST" request, check if updated_form data is valid
+    5. if valid, save updated form data
+    6. convert Markdown to HTML
+    7. redirect to renderhtml
+    """ 
+   
+    if form.has_changed() and request.method == "POST":
+        updated_form = NewForm(request.POST)
+        
+        if updated_form.is_valid():
+            title = updated_form.cleaned_data["title"]
+            content = updated_form.cleaned_data["content"]                    
+            
+            # convert Markdown to HTML
+            markdown_file = os.path.join(settings.BASE_DIR, f"entries/{title}.md")   
+            html_file = os.path.join(settings.BASE_DIR, f"templates/encyclopedia/{title}.html")
+            cli_command = f"python -m markdown2 {markdown_file} > {html_file}"
+            subprocess.run(cli_command, shell=True, check=True)
             
             util.save_entry(title, content)
-            return HttpResponseRedirect(reverse('renderhtml', args=[title]))
+                # with open(markdown_file, 'r') as md_file:
+                #     md_content = md_file.read()
+                #     html_content = markdown2.markdown(md_content)               
+                # with open(html_file, 'w') as html_file:
+                #     html_file.write(html_content)
+            return renderhtml(request, title)
+            #return HttpResponseRedirect(reverse('renderhtml', args=[title]))
         
-    else:   # render form with initial_data if invalid data /not "POST" request
-        return render(request, 'encyclopedia/edit_entry.html', context={'form': form, 'title': title} )
 
-        # if util.save_entry(title, content):               
-        #     # convert Markdown to HTML
-        #     markdown_file = f"entries/{title}.md"   
-        #     html_file = f"encyclopedia/templates/encyclopedia/{title}.html"
-        #     cli_command = f"python -m markdown2 {markdown_file} > {html_file}"
-        #     subprocess.run(cli_command, shell=True, check=True)
+        # render form with initial_data if invalid data /not "POST" request
+        # return render(request, 'encyclopedia/edit_entry.html', context={'form': form, 'title': title})
+
 
     # else:
         # return render(request, 'encyclopedia/edit_entry.html', context={'title' : title, 'form': form})
--- django/wiki/entries/CSS.md
+++ django/wiki/entries/CSS.md
@@ -1,3 +1,6 @@
-# CSS
-
-CSS is a language that can be used to add style to an [HTML](/wiki/HTML) page.
+# CSS
+
+
+- CSS is a language that can be used to add style to an [HTML](/wiki/HTML) page.
+- CSS stands for *Cascading Style Sheets*
+- CSS can be automated using SASS which stands for *Syntactically Awesome Style Sheets*

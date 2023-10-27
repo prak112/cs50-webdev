0230c00dab38c7a4b301e2850cc80896cfcf25d3
PrakGov <55034062+prak112@users.noreply.github.com>
2023-10-27 18:29:57 +0300

    add passenger-form, POST request

--- django/databases101/airline/flights/urls.py
+++ django/databases101/airline/flights/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("flights/<int:flight_id>", views.flight, name="flight"),
+    path("<int:flight_id>/add_passenger", views.add_passenger, name="add_passenger"),
 ]
--- django/databases101/airline/flights/views.py
+++ django/databases101/airline/flights/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Airport, Flight
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
     return render(request, "flights/index.html", {"flights": Flight.objects.all()})
 
+
 def flight(request, flight_id):
     flight = Flight.objects.get(pk=flight_id)
+    passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flight=flight).all()
+
     return render(request, "flights/flight.html", context={
         "flight": flight,
-        "passengers": flight.passengers.all(),
+        "passengers": passengers,
+        "non_passengers": non_passengers,
         })
+
+
+def add_passenger(request, flight_id):
+    if request.method == "POST":
+        # retrieve flight details
+        flight = Flight.objects.get(pk=flight_id)
+
+        # retrieve passenger_id from POST request
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+        
+        # add passenger to flight
+        passenger.flight.add(flight)
+
+        # redirect to modified flight page
+        return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
 

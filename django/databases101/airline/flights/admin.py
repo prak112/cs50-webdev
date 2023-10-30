6f79aa5084d4d9e54362f14a60a25c6d23614680
PrakGov <55034062+prak112@users.noreply.github.com>
2023-10-30 20:42:17 +0200

    filter selected/unselected flights

--- django/databases101/airline/flights/admin.py
+++ django/databases101/airline/flights/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from flights.models import Airport, Flight, Passenger
 
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_vertical = ("flight", )
+
 admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)

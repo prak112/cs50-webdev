8c641bbb3d3aa0030854a1e01d47116441fbb152
PrakGov <55034062+prak112@users.noreply.github.com>
2023-10-31 14:24:51 +0200

    add session variables

--- django/databases101/airline/airline/settings.py
+++ django/databases101/airline/airline/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Session Engine
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+# Session Storage
+SESSION_COOKIE_NAME = 'my_session_name'
